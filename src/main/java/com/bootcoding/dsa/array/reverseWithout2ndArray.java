9edfb5374e0142110100004404389218047ac39f
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-06 13:19:41 +0530

    Reverse Array without using second array

---
+++ src/main/java/com/bootcoding/dsa/array/reverseWithout2ndArray.java
@@ -0,0 +1,20 @@
+//Reverse Array without using second array
+package com.bootcoding.dsa.array;
+
+public class reverseWithout2ndArray {
+    public static void main(String[] args) {
+       int [] arr={10,20,30,40,50};
+       int temp=0;
+       for(int i=0;i<arr.length/2;i++){
+           temp=arr[i];
+           arr[i]=arr[arr.length-i-1];
+           arr[arr.length-i-1]=temp;
+
+        }
+       for(int j=0;j<arr.length;j++){
+           System.out.println(arr[j]);
+
+        }
+    }
+
+}
