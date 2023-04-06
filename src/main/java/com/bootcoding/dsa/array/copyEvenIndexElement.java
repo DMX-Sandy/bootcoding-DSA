07a53afd0e99dbe723130754002cf0fd7d5c87af
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-06 13:18:16 +0530

    Copy even index element of an array into another array.

---
+++ src/main/java/com/bootcoding/dsa/array/copyEvenIndexElement.java
@@ -0,0 +1,27 @@
+//Copy even index element of an array into another array.
+package com.bootcoding.dsa.array;
+
+public class copyEvenIndexElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        int index=0;
+        int [] arr2=new int [count];
+        for(int i =0;i<arr.length;i++){
+            if(i%2==0){
+                arr2[index++]=arr[i];
+
+            }
+        }
+        for (int i=0;i<count;i++){
+            System.out.print(arr2[i]+" ");
+        }
+    }
+}
