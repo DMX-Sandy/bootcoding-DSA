df319d09a69e012a639e0028ca646a3327f5f891
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-06 13:17:32 +0530

    To copy array Element.

---
+++ src/main/java/com/bootcoding/dsa/array/copyArrayElement.java
@@ -0,0 +1,12 @@
+package com.bootcoding.dsa.array;
+
+public class copyArrayElement {
+    public static void main(String[] args) {
+        int [] arr={10,20,30,50,90};
+        int [] arr2=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            arr2[i]=arr[i];
+            System.out.print(arr2+" ");
+        }
+    }
+}
