7e38952583db736b15e85805d6c599f9c1390ef5
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-13 17:29:32 +0530

    A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
    You are given an array of strings sentences, where each sentences[i] represents a single sentence.
    
    Return the maximum number of words that appear in a single sentence.
    
    Example 1:
    
    Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
    Output: 6
    Explanation:
    - The first sentence, "alice and bob love leetcode", has 5 words in total.
    - The second sentence, "i think so too", has 4 words in total.
    - The third sentence, "this is great thanks very much", has 6 words in total.
    Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.

---
+++ src/main/java/com/bootcoding/dsa/string/lc2114.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.string;
+
+public class lc2114 {
+    public static void main(String[] args) {
+        String [] sentences={"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+        int count=-1;
+        for(int i=0;i< sentences.length;i++){
+             String [] s1=sentences[i].split(" ");
+             if(s1.length>count){
+                 count=s1.length;
+             }
+
+         }
+        System.out.println(count);
+    }
+}
