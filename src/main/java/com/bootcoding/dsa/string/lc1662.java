7ec4a39f7fd073087b107455daad2ebbbacc35f6
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-13 17:27:37 +0530

    Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
    A string is represented by an array if the array elements concatenated in order forms the string.
    
    Example 1:
    
    Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
    Output: true
    Explanation:
    word1 represents string "ab" + "c" -> "abc"
    word2 represents string "a" + "bc" -> "abc"
    The strings are the same, so return true.

---
+++ src/main/java/com/bootcoding/dsa/string/lc1662.java
@@ -0,0 +1,23 @@
+package com.bootcoding.dsa.string;
+
+public class lc1662 {
+    public static void main(String[] args){
+              String [] word1={"a","bc"};
+              String [] word2={"ab","c"};
+                String sum1="";
+                String sum2="";
+                for(int i=0;i<word1.length;i++){
+                    sum1+=word1[i];
+                }
+                for(int j=0;j<word2.length;j++){
+                    sum2+=word2[j];
+                }
+
+                //return sum1.equals(sum2);
+                System.out.println(sum1.equals(sum2));
+
+            }
+
+        }
+
+
