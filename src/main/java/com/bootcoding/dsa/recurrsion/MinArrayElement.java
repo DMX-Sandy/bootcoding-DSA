34d5732fc4778d6485dbb9d623ccf988c6860d40
Prathamesh_developer <102478169+Prathmesh-developer@users.noreply.github.com>
2023-08-02 18:09:35 +0530

    Recurrsion

---
+++ src/main/java/com/bootcoding/dsa/recurrsion/MinArrayElement.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa.recurrsion;
+
+public class MinArrayElement {
+    public static int min(int [] a,int i,int min){
+        if(i==a.length){
+            return min;
+        }
+//        if(min>a[i]){
+//            min=a[i];
+//        }
+        Math.min(min,a[i]);
+        return min(a,++i,min);
+
+    }
+
+    public static void main(String[] args) {
+        int [] a={1,2,3,4,5};
+        int res=min(a,0,a[0]);
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/com/bootcoding/dsa/recurrsion/oddAssending.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa.recurrsion;
+
+public class oddAssending {
+    public static void odd(int n) {
+        if (n == 0) {
+            return;
+        }
+        odd(n - 1);
+        if (n % 2 != 0) {
+            System.out.println(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n=10;
+        odd(n);
+        System.out.print(" ");
+    }
+
+}
