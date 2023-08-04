f7983c9a3d5acaac410499f5877b78a5caa54814
PrathameshBhoge <prathameshbhoge.cool@gmail.com>
2023-08-04 15:51:52 +0530

    initial commit

---
+++ src/main/java/com/bootcoding/dsa/tree/Binarysearch/TreeNode.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.tree.Binarysearch;
+
+
+    class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+
+        TreeNode(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+
+}
