7d367514852eca018cbb0f9bd0494d14b2a8cf8b
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-01-26 00:51:00 +0530

    Leetcode

---
+++ src/main/java/com/bootcoding/leetcode75/array_string/lc58.java
@@ -0,0 +1,9 @@
+package com.bootcoding.leetcode75.array_string;
+
+public class lc58 {
+    public static void main(String[] args) {
+      String s = "Hello World";
+     String [] s1= s.split(" ");
+        System.out.println(s1[s1.length-1].length());
+    }
+}
