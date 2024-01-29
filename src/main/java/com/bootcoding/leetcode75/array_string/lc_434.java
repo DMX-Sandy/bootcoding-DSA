17190d3c34a6ecfd96642f17415e7d5840002f08
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-01-29 17:45:59 +0530

    Leetcode

---
+++ src/main/java/com/bootcoding/leetcode75/array_string/lc_434.java
@@ -0,0 +1,9 @@
+package com.bootcoding.leetcode75.array_string;
+
+public class lc_434 {
+    public static void main(String[] args) {
+        String s="Hello, my name is John";
+        String []s1=s.split(" ");
+        System.out.println(s1.length);
+    }
+}
