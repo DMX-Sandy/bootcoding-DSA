7b434458a8c9dff92bf0159f61ace6d039957b51
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-02-04 16:49:47 +0530

    practice toString

---
+++ src/main/java/com/bootcoding/dsa/string/Practice.java
@@ -0,0 +1,23 @@
+package com.bootcoding.dsa.string;
+
+import java.util.Arrays;
+
+public class Practice {
+    public static void main(String[] args) {
+//        String a= "Pratu";
+//        String b= new String("Pratu");
+//        System.out.println(a==b);
+        char a ='a';
+        char  b = 'b';
+        System.out.println(a+b);
+
+        int a1= 12;
+        System.out.println(a1+b);
+
+        String s1= "pratu";
+        System.out.println(s1+a);
+
+        String name = "Kunal Kushwaha Hello World";
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+}
---
+++ src/main/java/com/bootcoding/dsa/string/person.java
@@ -0,0 +1,32 @@
+package com.bootcoding.dsa.string;
+
+public class person {
+    public static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{name='" + name + "', age=" + age + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person("jhon",13);
+        System.out.println(p);
+
+    }
+}
