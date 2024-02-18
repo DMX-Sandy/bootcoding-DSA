e1165537431b868e404086d841bc531b605faec2
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-02-18 17:25:59 +0530

    practice

--- src/main/java/com/bootcoding/dsa/recurrsion/morning/First5.java
+++ src/main/java/com/bootcoding/dsa/recurrsion/morning/First5.java
@@ -5,9 +5,9 @@ public class First5 {
      print(1);
     }
     public static void print(int n){
-        if(n==6){
-            return;
-        }
+//        if(n==6){
+//            return;
+//        }
 
         System.out.println(n);
         print(++n);
