1b42869db920490972151671ea8f226fa8168f70
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-03-08 23:48:35 +0530

    practice

---
+++ src/main/java/com/bootcoding/dsa/array/OneDArray/asach.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa.array.OneDArray;
+
+public class asach {
+    int j=90;
+    public static void main(String[] args) {
+
+        print(90);
+
+
+    }
+    public static   void print(int pri){
+        int a =10;
+        int b =20;
+        do{
+            System.out.println(a);
+            a++;
+
+        }while(a<b);
+    }
+}
