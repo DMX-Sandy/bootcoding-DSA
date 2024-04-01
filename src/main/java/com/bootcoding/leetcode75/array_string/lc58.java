8273d66dbd813d8546541009ce812d70ea0bcfbc
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-04-01 19:36:38 +0530

    Initial commit

--- src/main/java/com/bootcoding/leetcode75/array_string/lc58.java
+++ src/main/java/com/bootcoding/leetcode75/array_string/lc58.java
@@ -3,7 +3,10 @@ package com.bootcoding.leetcode75.array_string;
 public class lc58 {
     public static void main(String[] args) {
       String s = "Hello World";
-     String [] s1= s.split(" ");
-        System.out.println(s1[s1.length-1].length());
+        System.out.println(lengthOfLastWord(s));
+    }
+    public static int lengthOfLastWord(String s){
+        String [] s1= s.split(" ");
+        return s1[s1.length-1].length();
     }
 }
