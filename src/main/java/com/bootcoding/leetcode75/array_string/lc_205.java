58f69a739dc221d9b10c49071fc50167d80ca7f4
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-04-02 13:50:59 +0530

    Initial commit

--- src/main/java/com/bootcoding/leetcode75/array_string/lc_205.java
+++ src/main/java/com/bootcoding/leetcode75/array_string/lc_205.java
@@ -9,7 +9,9 @@ public class lc_205 {
         System.out.println(isIsomorphic(s,t));
     }
     public static boolean isIsomorphic(String s, String t) {
+
         HashMap< Character , Character > map = new HashMap <>();
+
         for(int i = 0 ; i < s.length(); i++){
             if( !map.containsKey(s.charAt(i)) ){
                 if( map.containsValue(t.charAt(i)) ){
