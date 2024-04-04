0ecd1fa69339ab13b84b586815bd7ba463d95e3a
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-04-04 10:17:57 +0530

    Initial commit

---
+++ src/main/java/com/bootcoding/leetcode75/array_string/Lc_1614.java
@@ -0,0 +1,22 @@
+package com.bootcoding.leetcode75.array_string;
+
+public class Lc_1614 {
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+        System.out.println(maxDepth(s));
+    }
+    public static int maxDepth(String s) {
+        int count = 0;
+        int maxNum = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+                if (maxNum < count)
+                    maxNum = count;
+            } else if (c == ')') {
+                count--;
+            }
+        }
+        return maxNum;
+    }
+}
