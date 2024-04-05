df259fa7903353fa224116303a42e34a81d3ecf3
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-04-05 09:13:42 +0530

    Initial commit

---
+++ src/main/java/com/bootcoding/leetcode75/array_string/Lc_1544.java
@@ -0,0 +1,27 @@
+package com.bootcoding.leetcode75.array_string;
+
+public class Lc_1544 {
+    public static void main(String[] args) {
+            String s = "abBAcC";
+        System.out.println(makeGood(s));
+    }
+    public static String makeGood(String s) {
+        int endPosition = 0; // Represents the end position of the modified string
+        char[] charArray = s.toCharArray(); // Convert the string to a character array
+
+        // Loop through each character in the string
+        for (int currentPosition = 0; currentPosition < s.length(); currentPosition++) {
+            // Check if the current character can be removed
+            if (endPosition > 0 && Math.abs(charArray[currentPosition] - charArray[endPosition - 1]) == 32){
+                endPosition--; // Decrement the end position if the current character can be removed
+            }else {
+                // Otherwise, keep the current character and increment the end position
+                charArray[endPosition] = charArray[currentPosition];
+                endPosition++;
+            }
+        }
+
+        // Convert the modified character array to a string and return only the valid portion
+        return new String(charArray).substring(0, endPosition);
+    }
+}
