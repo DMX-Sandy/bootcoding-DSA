2ed0161f64fb7165ef5e99752a1481d867aef348
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-04-19 12:15:16 +0530

    Initial commit

--- src/main/java/com/bootcoding/leetcode75/array_string/Lc_200.java
+++ src/main/java/com/bootcoding/leetcode75/array_string/Lc_200.java
@@ -8,10 +8,8 @@ public class Lc_200 {
                 {'1','1','0','0','0'},
                 {'0','0','0','0','0'}
         };
-
         System.out.println(numIslands(grid));
     }
-
     public static int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) {
             return 0;
