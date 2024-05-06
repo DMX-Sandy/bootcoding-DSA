d9350a8178f94fab8412b773ad6dafe4ec40013a
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-05-06 22:55:02 +0530

    Initial commit

---
+++ src/main/java/com/bootcoding/TCS_NQT/Cube_Range.java
@@ -0,0 +1,17 @@
+package com.bootcoding.TCS_NQT;
+
+public class Cube_Range {
+    public static void main(String[] args) {
+        double n = 2;
+        double m = 5;
+        int k = 3;
+        System.out.println(Sumcube(n,m,k));
+    }
+    public static double Sumcube(double n, double m ,int k ){
+        int sum =0;
+        for(double i =n;i<=m;i++){
+            sum += Math.pow(i,k);
+        }
+        return sum ;
+    }
+}
