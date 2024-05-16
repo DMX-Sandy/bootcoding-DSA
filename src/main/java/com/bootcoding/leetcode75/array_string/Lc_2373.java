eff85385f91edae0b3895757bcd31473f88b4ca1
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-05-16 15:03:10 +0530

    Initial commit

--- src/main/java/com/bootcoding/leetcode75/array_string/Lc_2373.java
+++ src/main/java/com/bootcoding/leetcode75/array_string/Lc_2373.java
@@ -8,7 +8,6 @@ public class Lc_2373 {
     public static int[][] largestLocal(int[][] grid) {
         int n = grid.length;
         int[][] res = new int[n - 2][n - 2];
-
         for(int i = 1; i < n - 1; ++i) {
             for(int j = 1; j < n - 1; ++j) {
                 int temp = 0;
