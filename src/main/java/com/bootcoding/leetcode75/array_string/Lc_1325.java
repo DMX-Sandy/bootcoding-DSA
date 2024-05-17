7e0c14d8ed676432aa4fa8885306a3d3ef1c13f0
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-05-17 22:28:23 +0530

    Initial commit

---
+++ src/main/java/com/bootcoding/leetcode75/array_string/Lc_1325.java
@@ -0,0 +1,20 @@
+package com.bootcoding.leetcode75.array_string;
+
+import javax.swing.tree.TreeNode;
+
+public class Lc_1325 {
+    public static void main(String[] args) {
+       TreeNode root = {1,2,null,2,null,2};  int target = 2;
+    }
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root != null) {
+            root.left = removeLeafNodes(root.left, target);
+            root.right = removeLeafNodes(root.right, target);
+            if(root.left == null && root.right == null && root.val == target){
+                return null;
+            }
+            return root;
+        }
+        return null;
+    }
+}
