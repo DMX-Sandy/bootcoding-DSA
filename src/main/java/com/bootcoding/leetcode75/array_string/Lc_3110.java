2a8cbbd5eabb7929ce8a23b18d35cf024bb89e54
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-06-01 09:26:12 +0530

    Initial commit

---
+++ src/main/java/com/bootcoding/leetcode75/array_string/Lc_3110.java
@@ -0,0 +1,17 @@
+package com.bootcoding.leetcode75.array_string;
+
+public class Lc_3110 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(scoreOfString(s));
+    }
+
+        public static int scoreOfString(String s) {
+            int res = 0;
+            for (int i = 0; i < s.length() - 1; i++) {
+                res += Math.abs(s.charAt(i) - s.charAt(i + 1));
+            }
+            return res;
+        }
+
+}
